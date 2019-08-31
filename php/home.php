1a934f0547faf88d1c39f50e11e543e7c360c26b
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-08-31 20:48:54 +0530

    0.12

--- php/home.php
+++ php/home.php
@@ -2,16 +2,17 @@
 <?php 
 session_start();
 include("include/connection.php");
-
+if (!isset($_SESSION['email'])) {
+	header('Location:login.php');
+}
+else{
  ?>
 <html>
 <head>
 	<title>Home page</title>
 	<link rel="stylesheet" type="text/css" href="../css/home.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
 </head>
 <body>
@@ -101,7 +102,7 @@ include("include/connection.php");
 												<li>
 													<div class='rightside-chat'>
 														<span>$username<small>$date</small></span>
-														<p>$date</p>
+														<p>$msg_content</p>
 													</div>
 												</li>
 
@@ -111,9 +112,9 @@ include("include/connection.php");
 										else if($user_name == $receiver AND $username == $sender) {
 											echo "
 												<li>
-													<div class='rightside-chat'>
+													<div class=\"rightside-chat\">
 														<span>$username<small>$date</small></span>
-														<p>$date</p>
+														<p>$msg_content</p>
 													</div>
 												</li>
 
@@ -165,3 +166,4 @@ include("include/connection.php");
 
 </body>
 </html>
+<?php } ?>
